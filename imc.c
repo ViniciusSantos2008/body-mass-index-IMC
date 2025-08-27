65240eee466abe16a3b2bc80454b18792d776192
VINICIUS EDUARDO SANTOS ARAGÃO <vinicius.aragao@a.ucb.br>
2025-08-27 20:01:59 -0300

    Commit imc

--- imc.c
+++ imc.c
@@ -15,8 +15,21 @@ int main (){
     altura = altura/100;
     imc = peso / (altura * altura);
 
-    printf("O seu IMC é %f " , imc);
+    printf("O seu IMC é %.2f " , imc);
 
+    if (imc <= 18.5){
+        printf("Abaixo do peso! \n");
+    }   else if (imc <= 25.5){
+        printf("Peso normal (Ideal) \n");
+    }   else if (imc <= 29.5){
+        printf("Sobrepeso \n");
+    }   else if(imc <= 34.9){
+        printf("Obesidade grau I");
+    }   else if (imc <= 39.9){
+        printf("Obesidade grau II(Severa)");
+    }   else{
+        printf("Obesidade grau III(mórbida)");
+    }    
     return 0;
 
 }
